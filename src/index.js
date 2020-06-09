3c7999bcb6235f0e31a7aaa2789004385585cce5
Adrienne Dao <adriennedao@gmail.com>
2020-06-09 11:01:52 -0400

    passing props

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
         <div className="ui container comments">
-          <CommentDetail />   
+          <CommentDetail  author="Sam"/> 
+          <CommentDetail  author="Alex"/>
+          <CommentDetail  author="Jane"/>
         </div>
     );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(< App />, document.querySelector('#root'));
